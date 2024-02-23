99987c10d42836631316ebd711ee10311278c5ac
idsoid <125327978+idsoid@users.noreply.github.com>
2024-02-23 10:18:27 +0800

    settings

---
+++ SP4/Assets/PlayerPrefsManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerPrefsManager
+{
+    public static void Save(string key, float value)
+    {
+        PlayerPrefs.SetFloat(key, value);
+        PlayerPrefs.Save();
+    }
+    public static float Load(string key)
+    {
+        return PlayerPrefs.GetFloat(key);
+    }
+}
--- SP4/Assets/Resources/Audio/SettingsManager.cs
+++ SP4/Assets/Resources/Audio/SettingsManager.cs
@@ -14,22 +14,35 @@ public class SettingsManager : MonoBehaviour
 
     [SerializeField] private Volume volume;
     [SerializeField] private Slider brightSlider;
-    
+
+    private void Start()
+    {
+        masterSlider.value = PlayerPrefsManager.Load("MasterVolume");
+        sfxSlider.value = PlayerPrefsManager.Load("SFXVolume");
+        bgmSlider.value = PlayerPrefsManager.Load("BGMVolume");
+        brightSlider.value = PlayerPrefsManager.Load("BrightVolume");
+    }
     public void SetMasterVolume()
     {
         mixer.SetFloat("MasterVolume", masterSlider.value);
+        PlayerPrefsManager.Save("MasterVolume", masterSlider.value);
     }
     public void SetSFXVolume()
     {
         mixer.SetFloat("SFXVolume", sfxSlider.value);
+        PlayerPrefsManager.Save("SFXVolume", sfxSlider.value);
     }
     public void SetBGMVolume()
     {
         mixer.SetFloat("BGMVolume", bgmSlider.value);
+        PlayerPrefsManager.Save("BGMVolume", bgmSlider.value);
     }
     public void SetBrightVolume()
     {
         if (volume.profile.TryGet(out NoisePostProcess bright))
+        {
             bright.brightness.value = brightSlider.value;
+            PlayerPrefsManager.Save("BrightVolume", brightSlider.value);
+        }
     }
 }
